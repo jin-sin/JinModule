32911a073bbdd41b344d8fdb12be6ce52ce8f79a
jin_sin <jsin@appknot.com>
2021-07-02 10:25:18 +0900

    CNG api exception 의 코드랑 메시지 직접 접근가능하도록 변경

--- core-rx/src/main/java/com/appknot/core_rx/api/ApiResponseException.kt
+++ core-rx/src/main/java/com/appknot/core_rx/api/ApiResponseException.kt
@@ -6,7 +6,7 @@ import java.lang.RuntimeException
  *
  * @author Jin on 2019-06-14
  */
-class ApiResponseException(code: String, msg: ApiResponse.Msg) : RuntimeException(createErrorMessage(code, msg)) {
+class ApiResponseException(val code: String, val msg: ApiResponse.Msg) : RuntimeException(createErrorMessage(code, msg)) {
 
     constructor(response: ApiResponse) : this(response.code, response.msg)
 
