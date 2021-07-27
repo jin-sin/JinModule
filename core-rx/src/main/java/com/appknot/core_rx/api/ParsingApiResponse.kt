64060a2348aa96bbd709e6deebaace37714b57b4
Jin Gyuri Sin <jsin@appknot.com>
2021-07-27 12:42:09 +0900

    CNG: 데이터 파싱을 위한 누락된 코드 추가

--- core-rx/src/main/java/com/appknot/core_rx/api/ParsingApiResponse.kt
+++ core-rx/src/main/java/com/appknot/core_rx/api/ParsingApiResponse.kt
@@ -1,5 +1,7 @@
 package com.appknot.core_rx.api
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import okhttp3.Headers
 import okhttp3.ResponseBody
 import retrofit2.Response
@@ -9,7 +11,9 @@ import retrofit2.Response
  * appknot 컨벤션 기반 API 응답 구조
  * @author Jin Sin
  */
-abstract class ParsingApiResponse<T> {
+
+@Parcelize
+abstract class ParsingApiResponse<T> : Parcelable {
     lateinit var code: String
     lateinit var msg: Msg
     abstract var data: T?
