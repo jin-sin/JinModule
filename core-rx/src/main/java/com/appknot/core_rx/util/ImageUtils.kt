a6d2308b2ce6d5512e9ea528a9ed952e78f77c2c
Jin Gyuri Sin <jsin@appknot.com>
2021-09-02 12:18:54 +0900

    CNG: 글라이드 통신 의존성 버전 변경

--- core-rx/src/main/java/com/appknot/core_rx/util/ImageUtils.kt
+++ core-rx/src/main/java/com/appknot/core_rx/util/ImageUtils.kt
@@ -9,6 +9,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.GlideException
